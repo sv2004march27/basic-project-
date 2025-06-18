2fe6e27076e2e55e2111c07a77d039001de65ce9
shivam verma <129411326+sv2004march27@users.noreply.github.com>
2025-06-18 15:24:04 +0530

    Add files via upload

---
+++ calander.py
@@ -0,0 +1,11 @@
+import calendar
+
+def display_calendar():
+    year = int(input("Enter year: "))
+    month = int(input("Enter month (1-12): "))
+    cal = calendar.TextCalendar(calendar.SUNDAY)
+    month_calendar = cal.formatmonth(year, month)
+    print(month_calendar)
+
+
+display_calendar()
